38e5ca9f9070dc8c3a5fc505ea2b9aad13a2f3f6
Andre S <gymclothes@gmail.com>
2013-09-25 16:31:55 -0500

    updated UA bootstrap and add it to dependencies

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,12 +1,19 @@
 <?php
 
 /**
- * 
+ * Please copy the config below and place it on your /app/Config/bootstrap.php
+ * Remember to fill in the fields!
  */
     
-Configure::write('UrbanAirship.app.key', '');
-Configure::write('UrbanAirship.app.secret', '');
-Configure::write('UrbanAirship.app.masterSecret', '');
-Configure::write('UrbanAirship.push.url', 'https://go.urbanairship.com/api/push/');
+// Configure::write('UrbanAirship.key', '');
+// Configure::write('UrbanAirship.secret', '');
+// Configure::write('UrbanAirship.masterSecret', '');
+
+/**
+ * No need to edit!
+ *
+ */
+
+Configure::write('UrbanAirship.url', 'https://go.urbanairship.com/api/push/');
     
 ?>

bbf93b0c80f34c05168937d6616dc919dfe956ac
Andre S <gymclothes@gmail.com>
2013-10-17 10:36:37 -0500

    added Twilio data to bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -7,7 +7,11 @@
 
 if (!Configure::check('NotificationManager.UrbanAirship')) {
     Configure::write('NotificationManager.UrbanAirship.key', '');
-    Configure::write('NotificationManager.UrbanAirship.secret', '');
+    Configure::write('NotificationManager.UrbanAirship.master', '');
 }
-    
-?>
+
+if (!Configure::check('NotificationManager.Twilio')) {
+    Configure::write('NotificationManager.Twilio.sid', '');
+    Configure::write('NotificationManager.Twilio.token', '');
+    Configure::write('NotificationManager.Twilio.number', '');
+}

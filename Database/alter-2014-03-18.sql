4d6f3d7e6264346098762b064e0a0d584cb8e3eb
Andre S <gymclothes@gmail.com>
2014-03-18 15:58:01 -0500

    after

--- Database/alter-2014-03-18.sql
+++ Database/alter-2014-03-18.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `notifications` ADD `send_on` datetime DEFAULT NULL AFTER `data`, 
     ADD `timezone` varchar(128) DEFAULT 'UTC' AFTER `send_on`, 
-    ADD `condition` varchar(128) DEFAULT NULL `timezone`,
+    ADD `condition` varchar(128) DEFAULT NULL AFTER `timezone`,
     ADD `sent_on` datetime DEFAULT NULL AFTER `sent`;
